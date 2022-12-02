e9ef26f3a31ea8cc2b0c4f5f01b4da0fefd74b18
Christian Scaff <cscaff@ufl.edu>
2022-12-01 21:58:32 -0500

    Finished Merge/Quick Sort, made file input, need destructor, use new data file (comma issues).

--- Movie.cpp
+++ Movie.cpp
@@ -1,17 +1,19 @@
 #include "Movie.h"
 
-Movie::Movie(int _id, string _type, string _title, bool _adult, string _startYear, string _endYear, int _runtime, vector<string> _genres) {
+Movie::Movie(string _id, string _title, string _startYear, int _runtime, string _genre, double _avgRating, int _numVotes, double _runTimeNorm, double _avgRatingNorm, double _numVotesNorm) {
     this->id = _id;
-    this->type = _type;
     this->title = _title;
-    this->adult = _adult;
     this->startYear = _startYear;
-    this->endYear = _endYear;
     this->runtime = _runtime;
-    this->genres = _genres;
+    this->genre = _genre;
+    this->avgRating = _avgRating;
+    this->numVotes = _numVotes;
+    this->runTimeNorm = _runTimeNorm;
+    this->avgRatingNorm = avgRatingNorm;
+    this->numVotesNorm = _numVotesNorm;
 }
 
-int Movie::getId() {
+string Movie::getId() {
     return this->id;
 }
 
@@ -23,54 +25,18 @@ string Movie::getTitle() {
     return this->title;
 }
 
-bool Movie::getAdult() {
-    return this->adult;
-}
-
 string Movie::getStartYear() {
     return this->startYear;
-}
-
-string Movie::getEndYear() {
-    return this->endYear;
 }
 
 int Movie::getRuntime() {
     return this->runtime;
 }
 
-vector<string> Movie::getGenres() {
-    return this->genres;
+string Movie::getGenre() {
+    return this->genre;
 }
 
-int Movie::setId(int _id) {
-    this->id = _id;
-}
-
-string Movie::setType(string _type) {
-    this->type = _type;
-}
-
-string Movie::setTitle(string _title) {
-    this->title = _title;
-}
-
-bool Movie::setAdult(bool _adult) {
-    this->adult = _adult;
-}
-
-string Movie::setStartYear(string _startYear) {
-    this->startYear = _startYear;
-}
-
-string Movie::setEndYear(string _endYear) {
-    this->endYear = _endYear;
-}
-
-int Movie::setRuntime(int _runtime) {
-    this->runtime = _runtime;
-}
-
-vector<string> Movie::setGenres(vector<string> _genres) {
-    this->genres = _genres;
+double Movie::getScore(){
+    return this->score;
 }
--- Movie.h
+++ Movie.h
@@ -1,36 +1,33 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#pragma once
 
 using namespace std;
 
 class Movie
 {
     private:
-        int id;
+        string id;
         string type;
         string title;
         bool adult;
         string startYear;
-        string endYear;
         int runtime;
-        vector<string> genres;
+        string genre;
+        double avgRating;
+        int numVotes;
+        double runTimeNorm;
+        double avgRatingNorm;
+        double numVotesNorm;
+        double score;
     public:
-        Movie(int _id, string _type, string _title, bool _adult, string _startYear, string _endYear, int _runtime, vector<string> _genres);
-        int getId();
+        Movie(string _id, string _title, string _startYear, int _runtime, string _genre, double _avgRating, int _numVotes, double _runTimeNorm, double _avgRatingNorm, double _numVotesNorm);
+        string getId();
         string getType();
         string getTitle();
-        bool getAdult();
         string getStartYear();
-        string getEndYear();
         int getRuntime();
-        vector<string> getGenres();
-        int setId(int _id);
-        string setType(string _type);
-        string setTitle(string _title);
-        bool setAdult(bool _adult);
-        string setStartYear(string _startYear);
-        string setEndYear(string _endYear);
-        int setRuntime(int _runtime);
-        vector<string> setGenres(vector<string> _genres);
+        string getGenre();
+        double getScore();
 };
